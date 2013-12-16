0c2f9091de54abee6a25720cc96d45d957173bdf
Bryan <webmaster@warmoth.com>
2013-12-16 11:08:51 -0800

    Fourth Tut DONE

--- main.lua
+++ main.lua
@@ -5,9 +5,11 @@ display.setStatusBar( display.HiddenStatusBar );
 
 --Requires
 local graphics = require("graphics");
+
 --Game Variables
 local speed = 5;
-local right = true;
+local onGround = true;
+local wasOnGround = true;
 
 --Sprites
 local sheetOptions = {
@@ -42,8 +44,10 @@ local backgroundnear2 = display.newImage( "images/bgnear2.png" );
 backgroundnear2.x = 760;
 backgroundnear2.y = 160;
 
---Create a new group to hold our blocks
+--Create groups
 local blocks = display.newGroup();
+local player = display.newGroup();
+local screen = display.newGroup();
 
 --Ground
 local groundMin = 420;
@@ -54,7 +58,6 @@ for a = 1, 8, 1 do
 	isDone = false;
 	numGen = math.random(2);
 	local newBlock;
-	print(numGen);
 
 	if (numGen == 1 and isDone == false) then
 		newBlock = display.newImage( "images/ground1.png" );
@@ -73,41 +76,83 @@ for a = 1, 8, 1 do
 	newBlock.y = groundLevel;
 	blocks:insert(newBlock);
 end
---Hero
-local hero = display.newSprite(spriteSheet, sequnceData);
-hero.x = display.contentWidth / 2;
-hero.y = display.contentHeight / 2;
 
-hero:setSequence("running");
-hero:play();
+--monster
+local monster = display.newSprite(spriteSheet, sequnceData);
+monster.x = 110;
+monster.y = 200;
+monster.gravity = -6;
+monster.accel = 0;
+
+monster:setSequence("running");
+monster:play();
+
+--Collision Test
+local collisionRect = display.newRect( monster.x + 36, monster.y, 1, 70 );
+collisionRect.strokeWidth = 1;
+collisionRect:setFillColor( 140, 140, 140 );
+collisionRect:setStrokeColor( 180, 180, 180 );
+collisionRect.alpha = 0;
+
+--Load Groups
+screen:insert(backbackround);
+screen:insert(backgroundfar);
+screen:insert(backgroundnear1);
+screen:insert(backgroundnear2);
+screen:insert(blocks);
+screen:insert(monster);
+screen:insert(collisionRect);
 
 --Game Loop
 local function update( event )
 	updateBackgrounds();
+	updateSpeed();
+	updateMonster();
 	updateBlocks();
-	speed = speed + .05;
+	checkCollisions();
+end
 
-	--Move Hero
-	if (right) then
-		hero.x = hero.x + 3;
-	else
-		hero.x = hero.x - 3;
-	end
-
-	if (hero.x > 380) then
-		right = false;
-		hero.xScale = -1;
-	end
+--Functions
+function updateBackgrounds()
+	backgroundfar.x = backgroundfar.x - (speed / 55);
 	
-	if (hero.x < - 60) then
-		right = true;
-		hero.xScale = 1;
+	backgroundnear1.x = backgroundnear1.x - (speed / 5);
+	if (backgroundnear1.x < -239) then
+		backgroundnear1.x = 760;
+	end
+
+	backgroundnear2.x = backgroundnear2.x - (speed / 5);
+	if (backgroundnear2.x < -239) then
+		backgroundnear2.x = 760;
 	end
 end
 
+function updateSpeed()
+	speed = speed + .05;
+end
 
+function updateMonster()
+	if (onGround) then
+		if (wasOnGround) then
+		else
+			monster:setSequence( "running" );
+			monster:play();
+		end
+	else
+		monster:setSequence( "jumping" );
+		monster:play();
+	end
 
---Functions
+	if (monster.accel > 0) then
+		monster.accel = monster.accel - 1;
+	end
+
+	monster.y = monster.y - monster.accel;
+	monster.y = monster.y - monster.gravity;
+
+	collisionRect.y = monster.y;	
+end
+
 function updateBlocks()
 	for i = 1, blocks.numChildren, 1 do
 		if (i > 1) then
@@ -123,19 +168,39 @@ function updateBlocks()
 	end
 end
 
-function updateBackgrounds()
-	backgroundfar.x = backgroundfar.x - (speed / 55);
-	
-	backgroundnear1.x = backgroundnear1.x - (speed / 5);
-	if (backgroundnear1.x < -239) then
-		backgroundnear1.x = 760;
+function checkCollisions()
+	wasOnGround = onGround;
+
+	for i = 1, blocks.numChildren, 1 do
+		if (collisionRect.y - 10 > blocks[i].y - 170 and blocks[i].x - 40 < collisionRect.x and blocks[i].x + 40 > collisionRect.x) then
+			speed = 0;
+		end
 	end
 
-	backgroundnear2.x = backgroundnear2.x - (speed / 5);
-	if (backgroundnear2.x < -239) then
-		backgroundnear2.x = 760;
+	for i = 1, blocks.numChildren, 1 do
+		if (monster.y >= blocks[i].y - 170 and blocks[i].x < monster.x + 60 and blocks[i].x > monster.x - 60) then
+			monster.y = blocks[i].y - 171;
+			onGround = true;
+			break;
+		else
+			onGround = false;
+		end
+	end
+end
+
+--Listener functions
+function touched( event )
+	if (event.phase == "began") then
+		if (event.x < 241) then
+			if (onGround) then
+				monster.accel = monster.accel + 20;
+			end
+		end
 	end
 end
 
 --Timer
 timer.performWithDelay( 1, update, -1 );
+
+--Listeners
+Runtime:addEventListener( "touch", touched, -1 );
